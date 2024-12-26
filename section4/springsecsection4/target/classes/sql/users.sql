5b8f1c46ee1b1d8cc85b2126670dfb3d1f070634
niteshchaturvedi12 <40357700+niteshchaturvedi12@users.noreply.github.com>
2024-12-26 16:51:12 +0530

    Delete section4/springsecsection4/target directory

--- section4/springsecsection4/target/classes/sql/users.sql
+++
@@ -1,8 +0,0 @@
-insert ignore into `users` values('user', '{noop}EazyBytes@12345', '1');
-insert ignore into `users` values('admin', '{bcrypt}$2a$12$TFjuSjyfoCek1n2AAWIzUeGNele1uQirLQS.H/C9JkA6/6HxSP38C', '1');
-
-insert ignore into `authorities` values('user', 'read');
-insert ignore into `authorities` values('admin', 'admin');
-
-insert into `customer` (`email`, `pwd`, `role`) values ('user', '{noop}EazyBytes@12345', 'read');
-insert into `customer` (`email`, `pwd`, `role`) values ('admin', '{bcrypt}$2a$12$TFjuSjyfoCek1n2AAWIzUeGNele1uQirLQS.H/C9JkA6/6HxSP38C', 'admin');
